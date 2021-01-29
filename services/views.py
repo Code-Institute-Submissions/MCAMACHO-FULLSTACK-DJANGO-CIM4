ea32d1dcb0698e3da1251d77334be858f9d9d2f0
MACmidiDEV <macmidi.webdesigns@gmail.com>
2021-01-29 13:54:56 +0000

    cofig. Q search to navbar search form - searches by name or description

--- services/views.py
+++ services/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Service
 
 # Create your views here.
@@ -8,9 +11,20 @@ def all_services(request):
     """ A view to show all products, including sorting and search queries """
 
     services = Service.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('services'))           
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            services = services.filter(queries)
 
     context = {
         'services': services,
+        'search_term': query,
     }
 
     return render(request, 'services/services.html', context)
@@ -26,4 +40,3 @@ def service_detail(request, service_id):
     }
 
     return render(request, 'services/service_detail.html', context)
-    
