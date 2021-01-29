4e005500f6ea538919c5c2df05a42b0c74b5a0d8
MACmidiDEV <macmidi.webdesigns@gmail.com>
2021-01-29 15:36:10 +0000

    configured filtering services via categories, searches via navbar links

--- services/views.py
+++ services/views.py
@@ -2,36 +2,45 @@ from django.shortcuts import render, get_object_or_404
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Service
+from .models import Service, Category
 
 # Create your views here.
 
 
 def all_services(request):
-    """ A view to show all products, including sorting and search queries """
+    """ A view to show all services, including sorting and search queries """
 
     services = Service.objects.all()
     query = None
+    categories = None
 
     if request.GET:
+        if 'category' in request.GET:
+           categories = request.GET['category'].split(',')
+           services = services.filter(category__name__in=categories)
+           categories = Category.objects.filter(name__in=categories)
+
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
-                return redirect(reverse('services'))           
+                return redirect(reverse('services'))
+
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             services = services.filter(queries)
 
     context = {
         'services': services,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'services/services.html', context)
 
 
 def service_detail(request, service_id):
-    """ A view to show individual product details """
+    """ A view to show individual service details """
 
     service = get_object_or_404(Service, pk=service_id)
 
