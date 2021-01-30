f1d7dd56bf2672fdad675a58e12b26fddc503bc8
MACmidiDEV <macmidi.webdesigns@gmail.com>
2021-01-30 23:50:31 +0000

    coded JS to sort and back to top button

--- services/views.py
+++ services/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render, get_object_or_404
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from .models import Service, Category
+from django.db.models.functions import Lower
 
 # Create your views here.
-
 
 def all_services(request):
     """ A view to show all services, including sorting and search queries """
@@ -13,33 +12,51 @@ def all_services(request):
     services = Service.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = services.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            services = services.order_by(sortkey)
+            
         if 'category' in request.GET:
-           categories = request.GET['category'].split(',')
-           services = services.filter(category__name__in=categories)
-           categories = Category.objects.filter(name__in=categories)
-
+            services = request.GET['category'].split(',')
+            services = services.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('services'))
-
+            
             queries = Q(name__icontains=query) | Q(description__icontains=query)
-            services = services.filter(queries)
+            products = services.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'services': services,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'services/services.html', context)
 
 
-def service_detail(request, service_id):
+def service_detail(request, product_id):
     """ A view to show individual service details """
 
     service = get_object_or_404(Service, pk=service_id)
