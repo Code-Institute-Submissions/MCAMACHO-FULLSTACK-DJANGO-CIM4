bf043866e4f353191ec097f426e266f47089506a
MACmidiDEV <macmidi.webdesigns@gmail.com>
2021-02-01 07:13:02 +0000

    edit products and updateded add products

--- services/views.py
+++ services/views.py
@@ -4,6 +4,8 @@ from django.db.models import Q
 from .models import Service, Category
 from django.db.models.functions import Lower
 
+from .forms import ServiceForm
+
 # Create your views here.
 
 
@@ -56,6 +58,7 @@ def all_services(request):
 
     return render(request, 'services/services.html', context)
 
+
 def service_detail(request, service_id):
     """ A view to show individual service details """
 
@@ -67,12 +70,45 @@ def service_detail(request, service_id):
 
     return render(request, 'services/service_detail.html', context)
 
+
 def add_service(request):
     """ Add a service to the store """
-    form = ServiceForm()
+    if request.method == 'POST':
+        form = ServiceForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added service!')
+            return redirect(reverse('add_service'))
+        else:
+            messages.error(request, 'Failed to add service. Please ensure the form is valid.')
+    else:
+        form = ServiceForm()
+        
     template = 'services/add_service.html'
     context = {
         'form': form,
     }
 
-    return render(request, template, context)    
+    return render(request, template, context)
+
+
+def edit_service(request, service_id):
+    """ Edit a service in the store """
+    service = get_object_or_404(Service, pk=service_id)
+    if request.method == 'POST':
+        form = ServiceForm(request.POST, request.FILES, instance=service)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated service!')
+            return redirect(reverse('service_detail', args=[service.id]))
+        else:
+            messages.error(request, 'Failed to update service. Please ensure the form is valid.')
+    else:
+        form = ServiceForm(instance=service)
+        messages.info(request, f'You are editing {service.name}')
+
+    template = 'services/edit_service.html'
+    context = {
+        'form': form,
+        'service': service,
+    }
