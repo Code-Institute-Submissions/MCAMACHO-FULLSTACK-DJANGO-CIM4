d9d1d043791b05797f7a0adc58947d6f1c5c0b21
MACmidiDEV <macmidi.webdesigns@gmail.com>
2021-02-02 22:34:35 +0000

    set debug and secretkey env vars

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8iezp=m^l*mzhfb*$sq#*qkwfj!a7%pe1!6qt2-jtyc72d&9$)'
+SECRET_KEY = os.environ.get('SECRET_KEY','')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ecomm-ci-fsd.herokuapp.com','MACmidiDEV-ecomm-ci-fsd.herokuapp.com', 'localhost']
 
